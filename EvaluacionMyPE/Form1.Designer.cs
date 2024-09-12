1dffc3aa534bd666e1544b1d5a6ae51d79f33a00
efflores-21 <efflores@uamv.edu.ni>
2024-09-12 16:50:12 -0600

    Completed PracticaControlFlujo exercise

--- EvaluacionMyPE/Form1.Designer.cs
+++ EvaluacionMyPE/Form1.Designer.cs
@@ -28,20 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(127, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Suma";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(208, 12);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calcular ";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 35);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(10, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = ".";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 450);
+            this.ClientSize = new System.Drawing.Size(471, 86);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtInput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- EvaluacionMyPE/Form1.cs
+++ EvaluacionMyPE/Form1.cs
@@ -12,9 +12,49 @@ namespace EvaluacionMyPE
 {
     public partial class Form1 : Form
     {
+        private System.Collections.Generic.List<int> numeros;
+
         public Form1()
         {
             InitializeComponent();
+            numeros = new System.Collections.Generic.List<int>();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int numero;
+            if (int.TryParse(txtInput.Text, out numero))
+            {
+                if (numero >= 0)
+                {
+                    numeros.Add(numero);
+                    txtInput.Clear();
+                    txtInput.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Introduzca un número positivo o 0. Para finalizar, introduzca un número negativo.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Introduzca un número válido.");
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (numeros.Count == 0)
+            {
+                MessageBox.Show("No se han ingresado números.");
+                return;
+            }
+
+            int suma = numeros.Sum();
+            int mayor = numeros.Max();
+            int menor = numeros.Min();
+
+            lblResult.Text = $"Suma: {suma}\nMayor: {mayor}\nMenor: {menor}";
         }
     }
 }
